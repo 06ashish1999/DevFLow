cf60ffb3f344a07eb33dbecb16ab490a18d292d4
Sai Ashish Pinnamsetty <ideas.something@gmail.com>
2024-11-02 15:32:48 +0530

    ESLinst and Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-black">Welcome to nextJS course</h1>
+    <h1 className="text-3xl font-black text-white">Welcome to nextJS course</h1>
   );
 }
